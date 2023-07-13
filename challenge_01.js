70ebb8daf372c32016005718aac2de95d5c56071
Jenny <JenifferLeutert@gmx.de>
2023-07-13 12:53:38 +0200

    Anfang Summe

--- challenge_01.js
+++ challenge_01.js
@@ -9,3 +9,8 @@ Geben Sie die Summe der Zahlen in die Konsole aus:
 "Die Summe der Zahlen ist: summe"
 */
 
+let summe1, summe2;
+
+summe1 = prompt("Bitte Zahl eingeben:");
+summe2 = prompt("Bitte Zahl eingeben:");
+
